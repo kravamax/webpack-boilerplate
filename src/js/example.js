6184378b00ca12afec2f2ade4e496c6294d196d2
Maksym Kravchenko <maks.cross@gmail.com>
2022-02-17 15:19:37 +0200

    upd example.js

--- src/js/example.js
+++ src/js/example.js
@@ -1,3 +1,5 @@
 export const example =
   () => `Sensible webpack 5 boilerplate using Babel and PostCSS with a hot dev server 
   and an optimized production build.`
+
+console.log('This is boiler webpack')
